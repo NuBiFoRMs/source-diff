dee2a96b7d87fcc8b5a7dad2150582c6534a89b9
NuBiFoRM <nubiform@me.com>
2021-08-18 19:26:29 +0900

    batch

---
+++ src/main/java/com/nubiform/sourcediff/config/SchedulerConfig.java
@@ -0,0 +1,21 @@
+package com.nubiform.sourcediff.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+public class SchedulerConfig implements SchedulingConfigurer {
+
+    public static final int POOL_SIZE = 2;
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
+        threadPoolTaskScheduler.setThreadNamePrefix("scan-task-pool");
+        threadPoolTaskScheduler.initialize();
+        taskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+    }
+}
