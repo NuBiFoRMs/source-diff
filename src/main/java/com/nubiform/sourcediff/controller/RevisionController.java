27025734526ec8251c00ccf8ed006355a9cbed61
NuBiFoRM <nubiform@me.com>
2021-09-26 15:05:12 +0900

    revision controller

--- src/main/java/com/nubiform/sourcediff/controller/RevisionController.java
+++ src/main/java/com/nubiform/sourcediff/controller/RevisionController.java
@@ -1,4 +1,11 @@
 package com.nubiform.sourcediff.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RevisionController {
+
+    public void revision() {
+        
+    }
 }
