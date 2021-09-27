d4cf195a403d65b1c5f667a146bdb964447ee9dd
NuBiFoRM <nubiform@me.com>
2021-09-27 20:15:56 +0900

    revision service

--- src/main/java/com/nubiform/sourcediff/controller/RevisionController.java
+++ src/main/java/com/nubiform/sourcediff/controller/RevisionController.java
@@ -1,9 +1,15 @@
 package com.nubiform.sourcediff.controller;
 
+import com.nubiform.sourcediff.constant.SourceType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@RestController
+@Slf4j
+@RequiredArgsConstructor
+@Controller
 public class RevisionController {
 
     public static final String REVISION_URI = "/revision";
@@ -12,7 +18,9 @@ public class RevisionController {
     public static final String ANT_PATTERN = "/**";
 
     @GetMapping(REVISION_URI + REPOSITORY_PATH + SOURCE_TYPE_PATH + ANT_PATTERN)
-    public String revision() {
+    public String revision(@PathVariable String repository, @PathVariable SourceType sourceType) {
+        log.info("request: {}, repository: {}, sourceType: {}", REVISION_URI, repository, sourceType);
+
         return "revision";
     }
 }
