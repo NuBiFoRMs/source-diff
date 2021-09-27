41d631a51c811cd3efaac60cb05ccf4e8fb808de
NuBiFoRM <nubiform@me.com>
2021-09-27 18:34:35 +0900

    revision controller

--- src/main/java/com/nubiform/sourcediff/controller/RevisionController.java
+++ src/main/java/com/nubiform/sourcediff/controller/RevisionController.java
@@ -1,11 +1,18 @@
 package com.nubiform.sourcediff.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RevisionController {
 
-    public void revision() {
-        
+    public static final String REVISION_URI = "/revision";
+    public static final String REPOSITORY_PATH = "/{repository}";
+    public static final String SOURCE_TYPE_PATH = "/{sourceType}";
+    public static final String ANT_PATTERN = "/**";
+
+    @GetMapping(REVISION_URI + REPOSITORY_PATH + SOURCE_TYPE_PATH + ANT_PATTERN)
+    public String revision() {
+        return "revision";
     }
 }
