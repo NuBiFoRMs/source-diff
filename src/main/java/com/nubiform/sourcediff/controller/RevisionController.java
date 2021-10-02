467a94cf76ff5e6c00cc5ae4eef58e57b7247bc4
NuBiFoRM <nubiform@me.com>
2021-10-02 20:57:20 +0900

    revision

--- src/main/java/com/nubiform/sourcediff/controller/RevisionController.java
+++ src/main/java/com/nubiform/sourcediff/controller/RevisionController.java
@@ -1,10 +1,12 @@
 package com.nubiform.sourcediff.controller;
 
+import com.nubiform.sourcediff.config.AppProperties;
 import com.nubiform.sourcediff.constant.SourceType;
 import com.nubiform.sourcediff.repository.SvnLogRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -18,13 +20,17 @@ public class RevisionController {
     public static final String SOURCE_TYPE_PATH = "/{sourceType}";
     public static final String ANT_PATTERN = "/**";
 
+    private final AppProperties appProperties;
+
     private final SvnLogRepository svnLogRepository;
 
     @GetMapping(REVISION_URI + REPOSITORY_PATH + SOURCE_TYPE_PATH + ANT_PATTERN)
-    public String revision(@PathVariable String repository, @PathVariable SourceType sourceType) {
+    public String revision(@PathVariable String repository, @PathVariable SourceType sourceType, Model model) {
         log.info("request: {}, repository: {}, sourceType: {}", REVISION_URI, repository, sourceType);
 
         svnLogRepository.findAllByRepositoryAndSourceType(repository, sourceType.toString());
+
+        model.addAttribute("repositories", appProperties.getRepositories());
 
         return "revision";
     }
